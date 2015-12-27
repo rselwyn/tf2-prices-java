f30aed9a988795f65d0ae0f00d9fe000f912cb66
Robbie S <robbie@selwynsoftware.com>
2015-12-27 13:33:42 -0800

    Abstracted some JSOUP stuff and added some variables to the Item class

--- src/Item.java
+++ src/Item.java
@@ -5,11 +5,24 @@ import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 public class Item {
 	
+	/*
+	 * Jsoup Variables
+	 */
 	private Document doc;
 	private Elements price;
+	private final String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+	
+	/*
+	 * Item Variables
+	 */
+	private float priceInRef;
+	private float priceInKeys;
+	private float priceInBuds;
+	
 	
 	public Item(String url) throws IOException{
-		doc = Jsoup.connect("http://backpack.tf"+url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").timeout(1000).get();
+		//Set doc.  Set the userAgen
+		doc = Jsoup.connect("http://backpack.tf"+url).userAgent(userAgent).timeout(1000).get();
 		price = doc.getElementsByClass("item-panel-btns");
 		System.out.println(price.text());
 	}
--- src/Tests.java
+++ src/Tests.java
@@ -9,7 +9,7 @@ public class Tests {
 	@Test
 	public void test() {
 		try {
-			Item ref = new Item("/stats/Unique/Refined%20Metal/Tradable/Craftable");
+			Item ref = new Item("/stats/Unique/Mann%20Co.%20Supply%20Crate%20Key/Tradable/Craftable");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
