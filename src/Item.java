c8b4c10430166535f741e0deb06e88f188ea6d58
Robbie S <robbie@selwynsoftware.com>
2015-12-27 15:38:26 -0800

    Added all the initial parsing of the currency value

--- src/Item.java
+++ src/Item.java
@@ -19,8 +19,8 @@ public class Item {
 	private HashMap<String,Float> priceInCurrency = new HashMap<String,Float>();//A map containg the price in several real life currencys priceInCurrency["USD"] or priceInCurrency["GBP"]
 	private float priceInRef;
 	private float priceInKeys;
-	private float priceInBuds;
-	private String primaryInGameCurrency;
+	private float valueInUSD;
+	
 	
 	public Item(String url) throws IOException{
 		//Set doc.  Set the userAgen
@@ -30,7 +30,35 @@ public class Item {
 		System.out.println(price.text());
 	}
 	
-	public void decidePrimaryCurrency(){
-		
+	/*
+	 * Takes a string and decides whether the price 
+	 */
+	private void decidePrimaryCurrency(String which){
+		switch(which.split(" ")[1]){
+		case "ref": this.priceInRef = convertDashSeperated(which) ; break;
+		case "keys": this.priceInKeys = Float.parseFloat(which.split(" ")[0]) ; break;
+		default: this.valueInUSD = convertDollars(which.split(" ")[0]); break;
+		}
 	}
+	
+	/*
+	 * Takes two numbers seperated by a - and gets the average
+	 * @param	refVal: THe expression to parse
+	 * @return	the average of the two numbers
+	 */
+	private float convertDashSeperated(String refVal){
+		String[] vals = refVal.split(" ")[0].split("-");
+		return Float.parseFloat((vals[0]+vals[1]))/2;
+	}
+	
+	
+	/*
+	 * @param exprWithDollars:  The expression in the form of $XY-AB
+	 * @return the average of the two monetary values
+	 */
+	private float convertDollars(String exprWithDollars){
+		exprWithDollars = exprWithDollars.substring(1);
+		return convertDashSeperated(exprWithDollars);
+	}
+	
 }
